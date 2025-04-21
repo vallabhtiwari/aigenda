b3b77bb40624ce37c079cebe945212f93172e301
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-04-22 03:11:21 +0530

    added toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Providers } from "@/providers";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <Toaster />
         </body>
       </Providers>
     </html>
