e386fd637eb6de85798097e9515e579e42a22f73
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-04-22 03:12:52 +0530

    next-auth skeleton

---
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,36 @@
+import { AuthOptions, getServerSession } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+};
