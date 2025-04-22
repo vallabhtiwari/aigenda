488de85032f06770f5b8ef5fb939109327140171
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-04-23 03:32:32 +0530

    route to update todo

---
+++ src/app/api/todos/update/route.ts
@@ -0,0 +1,29 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { prisma } from "@/lib/db";
+import { UpdateTodoSchema } from "@/lib/zodSchemas";
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Invalid Session" }, { status: 401 });
+  }
+  const body = await req.json();
+  const safPayload = UpdateTodoSchema.safeParse(body);
+  if (!safPayload.data?.id) {
+    return NextResponse.json({ error: "Invalid Input" }, { status: 400 });
+  }
+  const { id, title, complete } = safPayload.data;
+  const todo = await prisma.todo.update({
+    where: {
+      id,
+      userEmail: session.user.email,
+    },
+    data: {
+      title,
+      complete,
+    },
+  });
+  return NextResponse.json({ todo });
+}
