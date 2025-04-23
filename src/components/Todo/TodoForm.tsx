2fe0c80340504d1bcb3bfc05fc00abbed9e51ce9
work-vallabhtiwari <163754005+work-vallabhtiwari@users.noreply.github.com>
2025-04-23 13:48:30 +0000

    Generic form to handle create/update

--- src/components/Todo/TodoForm.tsx
+++ src/components/Todo/TodoForm.tsx
@@ -2,30 +2,51 @@
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { AddTodoSchema } from "@/lib/zodSchemas";
 import { ZodError } from "zod";
 import { toast } from "sonner";
+import { Todo } from "@/lib/types";
 
-const AddTodo = () => {
+type TodoFormProps = {
+  todo?: Todo;
+  onSave?: (todo: Todo) => void;
+};
+
+export const TodoForm = ({ todo, onSave }: TodoFormProps) => {
   const [loading, setLoading] = useState(false);
   const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    setTitle(todo?.title || "");
+  }, [todo]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
   const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
     setLoading(true);
-    const url = "/api/todos/add";
     try {
+      let url, data;
       const parsedTitle = AddTodoSchema.parse(title);
+      if (todo) {
+        url = "/api/todos/update";
+        data = { id: todo.id, title: parsedTitle };
+      } else {
+        url = "/api/todos/add";
+        data = { title: parsedTitle };
+      }
       const resp = await axios.request({
         url,
         method: "POST",
-        data: { title: parsedTitle },
+        data,
       });
-      toast.success("Todo Added");
+      const savedTodo = resp.data.todo;
+      if (onSave) onSave(savedTodo);
       setTitle("");
+      todo ? toast.success("Todo Updated") : toast.success("Todo Added");
     } catch (e) {
       if (axios.isAxiosError(e)) {
         if (e.response?.data.error instanceof String)
@@ -40,7 +61,10 @@ const AddTodo = () => {
     setLoading(false);
   };
   return (
-    <div className="p-4 flex flex-col sm:flex-row justify-between items-center gap-2">
+    <form
+      onSubmit={handleSubmit}
+      className="p-4 flex flex-col sm:flex-row justify-between items-center gap-2"
+    >
       <Input
         type="text"
         name="todo"
@@ -51,13 +75,20 @@ const AddTodo = () => {
         className={cn("bg-input !text-3xl px-2 py-6")}
       />
       <Button
+        type="submit"
         className="w-full text-2xl sm:w-28 cursor-pointer px-2 py-6"
-        onClick={handleSubmit}
+        disabled={loading}
       >
-        Add
+        {todo ? "Update" : "Add"}
       </Button>
-    </div>
+      {todo && (
+        <Button
+          className="w-full text-2xl sm:w-28 cursor-pointer px-2 py-6"
+          variant="destructive"
+        >
+          Cancel
+        </Button>
+      )}
+    </form>
   );
 };
-
-export default AddTodo;
