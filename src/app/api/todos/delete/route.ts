76803927d1e3c964062d84582821e1916b645894
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-04-23 22:45:56 +0530

    route to handle delete

---
+++ src/app/api/todos/delete/route.ts
@@ -0,0 +1,24 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { prisma } from "@/lib/db";
+
+export async function DELETE(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Invalid Session" }, { status: 401 });
+  }
+  const { id } = await req.json();
+  if (!id) {
+    return NextResponse.json({ error: "Invalid Input" }, { status: 400 });
+  }
+  try {
+    await prisma.todo.delete({ where: { id } });
+    return NextResponse.json({ message: "Todo Deleted." });
+  } catch {
+    return NextResponse.json(
+      { error: "Something went wrong. Please try again" },
+      { status: 400 }
+    );
+  }
+}
