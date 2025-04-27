115358b1cbce65ba7253b37b0c629aec46753fcf
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-04-28 03:16:02 +0530

    initial setup for generating todos

--- src/app/actions/todoActions.ts
+++ src/app/actions/todoActions.ts
@@ -3,6 +3,10 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
 import { prisma } from "@/lib/db";
+import { GoogleGenAI, Type } from "@google/genai";
+import { SYSTEM_PROMPT } from "@/lib/prompts";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function getTodos() {
   try {
@@ -21,6 +25,81 @@ export async function getTodos() {
     return { todos };
   } catch (err) {
     console.error("Failed to fetch todos", err);
+    return { error: "Something went wrong. Please try again", todos: [] };
+  }
+}
+
+export async function generatedSuggestedTodos({
+  latitude,
+  longitude,
+}: {
+  latitude: number;
+  longitude: number;
+}) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.email) {
+      return { error: "Invalid session", todos: [] };
+    }
+    const recentTodos = await prisma.todo.findMany({
+      where: {
+        userEmail: session.user.email,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      take: 5,
+    });
+    const todos = recentTodos
+      .map(
+        (item, index) =>
+          `${index + 1}.${item.title}-${
+            item.complete ? "Complete" : "Incomplete"
+          }`
+      )
+      .join("\n");
+
+    const prompt = `These are user's recent todos:
+    ${todos}.
+    
+    This is user's current location:
+    Latitude:${latitude}, Longitude:${longitude}
+    
+    Generate 3 todos.`;
+
+    const response = await ai.models.generateContent({
+      model: process.env.GEMINI_MODEL!,
+      contents: "",
+      config: {
+        systemInstruction: SYSTEM_PROMPT,
+        temperature: 1.2,
+        topP: 0.9,
+        topK: 40,
+        responseMimeType: "application/json",
+        responseSchema: {
+          type: Type.ARRAY,
+          items: {
+            type: Type.OBJECT,
+            properties: {
+              prompt: {
+                type: Type.STRING,
+                description: "Prompt for the todo",
+                nullable: false,
+              },
+              title: {
+                type: Type.STRING,
+                description: "Title for the todo",
+                nullable: false,
+              },
+            },
+            required: ["prompt", "title"],
+          },
+        },
+      },
+    });
+    console.log(response.text);
+  } catch (err) {
+    console.log("Failed to generate suggestions", err);
     return { error: "Something went wrong. Please try again", todos: [] };
   }
 }
--- src/components/Todo/TodoListClient.tsx
+++ src/components/Todo/TodoListClient.tsx
@@ -5,10 +5,44 @@ import { useEffect } from "react";
 import { TodoItem } from "@/components/Todo/TodoItem";
 import { TodoForm } from "@/components/Todo/TodoForm";
 import { useTodoStore } from "@/store/todoStore";
+import axios from "axios";
+import { generatedSuggestedTodos } from "@/app/actions/todoActions";
 export const TodoListClient = ({ initialTodos }: { initialTodos: Todo[] }) => {
   const { todos, setInitialTodos } = useTodoStore();
   useEffect(() => {
     setInitialTodos(initialTodos);
+    async function fetchSuggestions() {
+      try {
+        const pos = await new Promise<GeolocationPosition>((resolve, reject) =>
+          navigator.geolocation.getCurrentPosition(resolve, reject)
+        );
+        const { latitude, longitude } = pos.coords;
+        console.log(pos);
+        console.log(latitude, longitude);
+        const suggestions = await generatedSuggestedTodos({
+          latitude,
+          longitude,
+        });
+        if (suggestions)
+          suggestions.forEach((prompt: string) => {
+            const id = crypto.randomUUID();
+            const todo = {
+              id,
+              title: prompt,
+              complete: false,
+              prompt,
+              userEmail: null,
+              createdAt: null,
+              updatedAt: null,
+            };
+            // addSuggestedTodo(todo)
+          });
+      } catch (error) {
+        console.error("Failed to fetch AI suggestions", error);
+      }
+    }
+
+    fetchSuggestions();
   }, [initialTodos, setInitialTodos]);
 
   return (
